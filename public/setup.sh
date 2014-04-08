c6e3d2256b6108f396045cd2ac32c86b5c94eeaa
K. Arthur Endsley <kaendsle@mtu.edu>
2014-04-08 16:52:16 -0400

    Updated setup process

--- public/setup.sh
+++ public/setup.sh
@@ -1,6 +1,9 @@
+# Downloads static files from server and puts them in place here
 wget http://spatial.mtri.org/flux/shared/moment/moment.min.js -O /usr/local/project/flux-server/node_modules/moment/moment.min.js
 wget http://spatial.mtri.org/flux/shared/topojson/topojson.min.js -O /usr/local/project/flux-server/node_modules/topojson/topojson.min.js
 wget http://spatial.mtri.org/flux/media/world-110m.json -O /usr/local/project/flux-server/public/media/world-110m.json
 wget http://spatial.mtri.org/flux/media/world-110m.json -O /usr/local/project/flux-server/public/media/world-110m.json
 wget http://spatial.mtri.org/flux/media/out.json -O /usr/local/project/flux-server/public/media/out.json
+wget http://spatial.mtri.org/flux/media/us_states.topo.json -O /usr/local/project/flux-server/public/media/us_states.topo.json
+wget http://spatial.mtri.org/flux/media/stats.json -O /usr/local/project/flux-server/public/media/stats.json
 wget http://spatial.mtri.org/flux/media/2014_01_Endsley_OCO-2_Pasadena_example.kmz -O /usr/local/project/flux-server/public/media/2014_01_Endsley_OCO-2_Pasadena_example.kmz
