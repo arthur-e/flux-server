b18b7f6081f08c0d5ebf4e3759ac6edd1fd9488c
K. Arthur Endsley <kaendsle@mtu.edu>
2014-05-20 16:47:03 -0400

    Refactored scenarios.json endpoint so that the 'pretty' title of a scenario is also serialized in a bulk request (no GET arguments)

--- api/scenario.js
+++ api/scenario.js
@@ -3,20 +3,21 @@ var numeric                 = require('numeric');
 var _                       = require('underscore');
 
 function scenario (req, res) {
-    if (!(req.params.scenario || req.query.scenario)) {
-        // Only return the list of scenario names
+    var scn = req.params.scenario || req.query.scenario;
+    var scns;
+
+    // Only return the list of scenario names
+    if (!scn) {
         if (!core.SCENARIOS) {
             return res.send(404, "Not Found");
         } else {
-            return res.send(_.map(core.SCENARIOS, function (name) {
-                return {
-                    _id: name
-                }
-            }));
+            scns = [];
+            _.each(core.SCENARIOS, function (name) {
+                scns.push(_.pick(core.METADATA[name], '_id', 'title'));
+            });
+            return res.send(scns);
         }
     }
-
-    var scn = req.params.scenario || req.query.scenario;
 
     if (!(scn in core.METADATA) || core.METADATA[scn] === undefined) {
         return res.send(404, 'Not Found');
